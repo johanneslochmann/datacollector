23f9199cc1ff29591cd718f6118a82119a17653d
Johannes Lochmann <johannes.lochmann@gmail.com>
2015-11-06 09:38:28 +0100

    fixed column titles

--- campaignmanagementwidget.cxx
+++ campaignmanagementwidget.cxx
@@ -9,14 +9,19 @@
 CampaignManagementWidget::CampaignManagementWidget(QWidget *p)
     : TableManagementWidget(p, "core.campaign", QObject::tr("Campaigns"))
 {
-    model()->setRelation(model()->fieldIndex("project_id"),
-                         QSqlRelation("core.project", "id", "name"));
+    model()->setRelation(model()->fieldIndex("project_id"), QSqlRelation("core.project", "id", "name"));
 
     view()->setItemDelegate(new QSqlRelationalDelegate(view()));
     view()->setItemDelegateForColumn(model()->fieldIndex("start_date"), new DateEditDelegate(view()));
     view()->setItemDelegateForColumn(model()->fieldIndex("end_date"), new DateEditDelegate(view()));
 
     view()->hideColumn(model()->fieldIndex("id")); // hide id column
+
+    model()->setHeaderData(model()->fieldIndex("description"), Qt::Horizontal, tr("Description"));
+    model()->setHeaderData(model()->fieldIndex("name"), Qt::Horizontal, tr("Campaign"));
+    model()->setHeaderData(1, Qt::Horizontal, tr("Project"));
+    model()->setHeaderData(model()->fieldIndex("start_date"), Qt::Horizontal, tr("Begin"));
+    model()->setHeaderData(model()->fieldIndex("end_date"), Qt::Horizontal, tr("End"));
 
     model()->select();
 }
