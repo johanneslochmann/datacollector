6091b62f2a048a37047bd2a0c0f51c0f7849211a
Johannes Lochmann <johannes.lochmann@gmail.com>
2015-11-06 09:38:37 +0100

    fixed column titles

--- probandmanagementwidget.cxx
+++ probandmanagementwidget.cxx
@@ -1,5 +1,7 @@
 #include "probandmanagementwidget.hxx"
 
+#include <QSqlRelationalDelegate>
+
 #include "sqltablemodel.hxx"
 #include "tableview.hxx"
 #include "dateeditdelegate.hxx"
@@ -8,7 +10,18 @@ ProbandManagementWidget::ProbandManagementWidget(QWidget *p)
     : TableManagementWidget(p, tr("core.proband"), QObject::tr("Probands"))
 {
     view()->hideColumn(model()->fieldIndex("id")); // hide id column
+    model()->setRelation(model()->fieldIndex("sex_id"), QSqlRelation("core.sex", "id", "name"));
+
+    view()->setItemDelegate(new QSqlRelationalDelegate(view()));
     view()->setItemDelegateForColumn(model()->fieldIndex("birthday"), new DateEditDelegate(view()));
+
+    model()->setHeaderData(model()->fieldIndex("external_id"), Qt::Horizontal, tr("External ID"));
+    model()->setHeaderData(model()->fieldIndex("birthday"), Qt::Horizontal, tr("Birthday"));
+    model()->setHeaderData(model()->fieldIndex("height_in_cm"), Qt::Horizontal, tr("Height [cm]"));
+    model()->setHeaderData(model()->fieldIndex("description"), Qt::Horizontal, tr("Comment"));
+    model()->setHeaderData(model()->fieldIndex("first_name"), Qt::Horizontal, tr("First Name"));
+    model()->setHeaderData(model()->fieldIndex("surname"), Qt::Horizontal, tr("Surname"));
+    model()->setHeaderData(4, Qt::Horizontal, tr("Sex"));
 
     model()->select();
 }
