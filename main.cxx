52cb7f7863142d0688f11c4924ecf1c5575295ad
Johannes Lochmann <johannes.lochmann@gmail.com>
2015-11-07 07:07:45 +0100

    set application metadata

--- main.cxx
+++ main.cxx
@@ -3,6 +3,13 @@
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setApplicationName("datacollector");
+    QCoreApplication::setApplicationVersion("0.1");
+    QCoreApplication::setOrganizationName("jolodev");
+    QCoreApplication::setOrganizationDomain("johanneslochmann.blogspot.com");
+    QApplication::setApplicationDisplayName(QObject::tr("%1 V. %2")
+                                            .arg(QCoreApplication::applicationName())
+                                            .arg(QCoreApplication::applicationVersion()));
     DataCollector a(argc, argv);
     MainWindow w;
     w.showMaximized();
